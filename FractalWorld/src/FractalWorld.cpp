43740d26323e74f1b3076f97e4678b3dfa54c20b
msolski <michaelavsolski@rogers.com>
2017-11-25 15:35:28 -0500

    Boilerplate stuff

--- FractalWorld/src/FractalWorld.cpp
+++ FractalWorld/src/FractalWorld.cpp
@@ -1,20 +1,32 @@
-//============================================================================
-// Name        : FractalWorld.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+#include <GL/glut.h>
 #include <iostream>
 #include "DiamondSquare.hpp"
-
+#include "Camera.hpp"
 using namespace std;
 
 //Hello -Paul
+//Sup -Michael
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+// Global Variables
+GLint winWidth = 800, winHeight = 800;
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(winWidth, winHeight);
+	glutCreateWindow("Fractal World - Paul Hohbaum and Michael Solski");
+
+	//glewInit(); // for using GSLS
+
+	/*
+	init();
+	glutDisplayFunc(display);
+	glutMotionFunc(mouseMotion);
+	glutMouseFunc(mouseAction);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	*/
+	glutMainLoop();
 	return 0;
 
 }
